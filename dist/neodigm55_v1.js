ef6caba03de46c0c4301dba3a6fb02acbeaf7fc1
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2021-10-23 17:34:20 -0500

    Update neodigm55_v1.js

--- dist/neodigm55_v1.js
+++ dist/neodigm55_v1.js
@@ -1,10 +1,3 @@
-<neodigm-snackbar class="l-snackbar" role="alert"><!--  Component Begin  -->
-  <section id="js-snackbar__id" class="snackbar__cont snackbar__cont--hide" aria-live="polite" aria-atomic="true">
-      <div class="snackbar__progbar"></div>
-      <p class="snackbar__msg"></p>
-  </section>
-</neodigm-snackbar><!--  Component End  -->
-
 var neodigmSnack = (function(_d, eID) {
     var _nTimeout = 5400, _aQ = [];
     var _eSb = _d.getElementById(eID),
