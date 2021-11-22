73305caa03ebb5f7e07fefbd9030f526492d6d02
neodigm <3151842+neodigm@users.noreply.github.com>
2021-11-21 21:27:33 -0600

    create-account-spec

--- dist/neodigm55_v1_1.js
+++ dist/neodigm55_v1_1.js
@@ -111,7 +111,7 @@ const neodigmSodaPop = ( ( _d, _aQ ) =>{
       },
       shake: function(){
         let iT = 188;
-        for(let x=1; x<=8; x++){
+        for(let x=1; x<=10; x++){
           setTimeout(function(){ eSoda.classList.add( "ndsp__opened--shake" ); }, ( iT * x ));
           setTimeout(function(){ eSoda.classList.remove( "ndsp__opened--shake" ); }, ( iT * x ) + ( iT/2 ));
         }
