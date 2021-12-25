e8fb53b3f745d53633c32d948080417bf5844d0e
neodigm <3151842+neodigm@users.noreply.github.com>
2021-12-25 17:44:20 -0600

    Metronome_logic_1_6_delta

--- dist/neodigm55_v1_6.js
+++ dist/neodigm55_v1_6.js
@@ -301,7 +301,7 @@ const neodigmMarquee = ( ( _d, _aQ, _t ) =>{
       pause: function(){ if( bIsInit ){ bIsPause = true;  return neodigmMarquee; } },
       play:  function(){ if( bIsInit ){ bIsPause = false; return neodigmMarquee; } }
     }
-  })( document, ["neodigm-marquee", "neodigmMarqueeText"], 120);
+  })( document, ["neodigm-marquee", "neodigmMarqueeText"], 240);
 
 //  Neodigm 55 UX A11Y skip Begin  //
 //  Neodigm 55 UX Confetti Begin  //
