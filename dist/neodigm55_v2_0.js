9e7494d1ee452a5f20c6bf94c9cef00f14f9938e
neodigm <3151842+neodigm@users.noreply.github.com>
2022-05-12 11:42:58 -0500

    toast_height

--- dist/neodigm55_v2_0.js
+++ dist/neodigm55_v2_0.js
@@ -406,25 +406,20 @@ class NeodigmClaireAtom{
   draw(){
     if( !this.complete ) this.size = this.size + ( this.nMax * this.nInverse ) / 12
 
-    this.dotCtx.globalCompositeOperation = "source-over";  //  "destination-atop"
-    this.dotCtx.beginPath()
-    this.dotCtx.arc( neodigmUtils.f02x( this.nMax ), neodigmUtils.f02x( this.nMax ), neodigmUtils.f02x( this.nMax ) + 8, 0, 2 * Math.PI, false)
-    this.dotCtx.closePath()
-    this.dotCtx.fill()
-
-    this.dotCtx.globalCompositeOperation = "destination-atop";  //  "destination-atop"
-    this.dotCtx.beginPath()
-    this.dotCtx.arc( neodigmUtils.f02x( this.nMax ), neodigmUtils.f02x( this.nMax ), neodigmUtils.f02x( this.nMax ) + 8, 0, 2 * Math.PI, false)
-    this.dotCtx.closePath()
-    this.dotCtx.fill()
 
     this.dotCtx.globalCompositeOperation = "destination-out"
     this.dotCtx.beginPath()
     this.dotCtx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, false)
     this.dotCtx.closePath()
     this.dotCtx.fill()
-    this.dotCtx.globalCompositeOperation = "destination-atop"
 
+    this.dotCtx.beginPath()
+    this.dotCtx.arc( neodigmUtils.f02x( this.nMax ), neodigmUtils.f02x( this.nMax ), neodigmUtils.f02x( this.nMax ) + 8, 0, 2 * Math.PI, false)
+    this.dotCtx.closePath()
+    this.dotCtx.lineWidth = neodigmUtils.f02x( 12 ) + 2;
+    this.dotCtx.strokeStyle = "#edba08";
+    this.dotCtx.stroke();
+    this.dotCtx.globalCompositeOperation = "destination-atop";  //  "destination-atop"
 
     this.complete = (this.size >= (  this.nMax * 1.4 ) )
     return !this.complete
