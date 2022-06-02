edf00e85e2f1215681d3a10f5b31170467847eb7
neodigm <3151842+neodigm@users.noreply.github.com>
2022-06-02 12:55:54 -0500

    n55Claire

--- dist/neodigm55_v2_1.js
+++ dist/neodigm55_v2_1.js
@@ -763,12 +763,12 @@ function doDOMContentLoaded(){
 <neodigm-sodapop-scrim-close>
     <svg fill="#DD4124" height="48" viewBox="0 0 22 22" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
 </neodigm-sodapop-scrim-close>
-<neodiigm-snack class="l-snackbar" role="alert">
+<neodigm-snack class="l-snackbar" role="alert">
     <section id="js-snackbar__id" class="snackbar__cont snackbar__cont--hide" aria-live="polite" aria-atomic="true">
         <div class="snackbar__progbar"></div>
         <p class="snackbar__msg"></p>
     </section>
-</neodiigm-snack>`;
+</neodigm-snack>`;
   let eMU = document.createElement("output");
   eMU.innerHTML = neodigmMU;
   document.body.appendChild(eMU);
