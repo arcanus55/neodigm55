1f3fa34667d3e137285f1b081a55bc501e247604
neodigm <3151842+neodigm@users.noreply.github.com>
2022-06-12 12:57:03 -0500

    gobsmacked

--- dist/neodigm55_v2_1.js
+++ dist/neodigm55_v2_1.js
@@ -160,7 +160,7 @@ class NeodigmSodaPop {
           }
         }, false)
         this._d.body.addEventListener("mouseleave", (ev) => {
-          if( this.fOnBeforeUserExit.length && !sessionStorage.getItem( "n55_userExit" ) ) this.fOnBeforeUserExit.forEach( (f)=> f())
+          if( this.fOnBeforeUserExit.length && !sessionStorage.getItem( "n55_userExit" ) ) this.fOnBeforeUserExit[0]()//this.fOnBeforeUserExit.forEach( (f)=> f())
           sessionStorage.setItem( "n55_userExit", Date.now() )
         })
         this.bIsInit = true
@@ -170,7 +170,7 @@ class NeodigmSodaPop {
       if(this.bIsOpen) this.close(true)
       this.eTmpl = this._d.getElementById(_sId)
       if(this.bIsInit && this.eTmpl && this.eScrim) {
-          if(this.fOnBeforeOpen.length) this.fOnBeforeOpen.forEach( (f)=> f())
+          if(this.fOnBeforeOpen.length) this.fOnBeforeOpen[0]()//this.fOnBeforeOpen.forEach( (f)=> f())
           this.bIsModal = (this.eTmpl.dataset.n55SodapopModal == "true")
           if(this.bIsModal) {
             this.eClose.classList.add("ndsp__modal")
@@ -198,14 +198,14 @@ class NeodigmSodaPop {
           this.bIsFS = ( this.eTmpl.dataset.n55SodapopFullscreen == "true" )
           if( this.bIsFS ) _d.body.requestFullscreen()
           this.bIsOpen = true;
-          if(this.fOnAfterOpen.length) this.fOnAfterOpen.forEach( (f)=> f())
+          if(this.fOnAfterOpen.length) this.fOnAfterOpen[0]()//this.fOnAfterOpen.forEach( (f)=> f())
           if( window.dataLayer && neodigmOpt.N55_GTM_DL_POP_OPEN ) window.dataLayer.push( {"event": neodigmOpt.N55_GTM_DL_POP_OPEN, "id": _sId } )
       }
       return neodigmSodaPop
     }
     close( _bFast ) {
         if(this.bIsInit && this.bIsOpen) {
-            if(this.fOnClose.length) this.fOnClose.forEach( (f)=> f())
+            if(this.fOnClose.length) this.fOnClose[0]()//this.fOnClose.forEach( (f)=> f())
             this.eClose.dataset.n55SodapopScrim = "closed"
             if(_bFast) {
                 this.eSoda.remove()
@@ -447,7 +447,7 @@ class NeodigmClaireAtomOn{
 }
 
 class NeodigmClaireAtomOff{
-  constructor(x, y, ctx, cnvIdx, cnvMax){
+  constructor(x, y, ctx, cnvIdx, cnvMax, sceen="circle"){
     this.complete = false; this.size = 1
     this.x = x; this.y = y;
     this.dotCtx = ctx; this.cnvIdx = cnvIdx
