a5bdf1ed0c136284d80bc66740aacf885a660419
neodigm <3151842+neodigm@users.noreply.github.com>
2022-12-05 14:45:53 -0600

    resizeEvent

--- dist/neodigm55__v2_4.js
+++ dist/neodigm55__v2_4.js
@@ -89,6 +89,7 @@ const neodigmUtils = ( ( _d ) =>{
       document[ qContext ].addEventListener( "touchstart", function( ev ){
         if( !neodigmOpt.N55_APP_STATE.FIRST_TAP ){ neodigmOpt.N55_APP_STATE.FIRST_TAP = true }
       })
+      window.addEventListener("resize", function( ev ){ if( neodigmOpt.neodigmCarousel ) neodigmCarousel.init() })
     },
     capFirst: s => (s && s[0].toUpperCase() + s.slice(1)) || ""
   }
