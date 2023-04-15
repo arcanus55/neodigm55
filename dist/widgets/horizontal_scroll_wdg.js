d503c06e79e174449a2a9e43f304b0b6ccac62e7
neodigm <3151842+neodigm@users.noreply.github.com>
2023-04-15 08:40:42 -0500

    widget

---
+++ dist/widgets/horizontal_scroll_wdg.js
@@ -0,0 +1,49 @@
+<script>
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+/* Main navigation */
+let panelsSection = document.querySelector("#panels"),
+  panelsContainer = document.querySelector("#panels-container"),
+  tween;
+document.querySelectorAll(".anchor").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    let targetElem = document.querySelector(e.target.getAttribute("href")),
+      y = targetElem;
+    if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
+      let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+        totalMovement = (panels.length - 1) * targetElem.offsetWidth;
+      y = Math.round(
+        tween.scrollTrigger.start +
+          (targetElem.offsetLeft / totalMovement) * totalScroll
+      );
+    }
+    gsap.to(window, {
+      scrollTo: {
+        y: y,
+        autoKill: false
+      },
+      duration: 1
+    });
+  });
+});
+
+/* Panels */
+const panels = gsap.utils.toArray("#panels-container .panel");
+tween = gsap.to(panels, {
+  xPercent: -100 * (panels.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#panels-container",
+    pin: true,
+    start: "top top",
+    scrub: 1,
+    snap: {
+      snapTo: 1 / (panels.length - 1),
+      inertia: false,
+      duration: { min: 0.1, max: 0.1 }
+    },
+    end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
+  }
+});
+</script>
