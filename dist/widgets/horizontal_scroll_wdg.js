912bc47d169a839ce01b7c11593cc31d1ab51338
neodigm <3151842+neodigm@users.noreply.github.com>
2023-04-15 13:05:17 -0500

    widget

--- dist/widgets/horizontal_scroll_wdg.js
+++ dist/widgets/horizontal_scroll_wdg.js
@@ -1,8 +1,10 @@
-gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
-let regionContainer = document.querySelector("#region-container");
-let tween;
-const region = gsap.utils.toArray("#region-container .panel");
-tween = gsap.to(region, {
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger)
+let elRegCont = document.querySelector("n55-wdg-horz-scrl #region-container")
+if( elRegCont ){
+  let tween;
+  const region = gsap.utils.toArray("#region-container .panel")
+  elRegCont.style.width = "1000%";
+  tween = gsap.to(region, {
   xPercent: -100 * (region.length - 1),
   ease: "none",
   scrollTrigger: {
@@ -15,6 +17,7 @@ tween = gsap.to(region, {
       inertia: false,
       duration: { min: 0.1, max: 0.1 }
     },
-    end: () => "+=" + (regionContainer.offsetWidth - innerWidth)
+    end: () => "+=" + (elRegCont.offsetWidth - innerWidth)
   }
-});
+  });
+}
