3a08935625bf3b3624861ff837c547d9e3ceda3d
neodigm <3151842+neodigm@users.noreply.github.com>
2023-04-15 11:39:06 -0500

    widget

--- dist/widgets/horizontal_scroll_wdg.js
+++ dist/widgets/horizontal_scroll_wdg.js
@@ -1,47 +1,20 @@
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
-
-/* Main navigation */
-let panelsSection = document.querySelector("#panels"),
-  panelsContainer = document.querySelector("#panels-container"),
-  tween;
-document.querySelectorAll(".anchor").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    let targetElem = document.querySelector(e.target.getAttribute("href")),
-      y = targetElem;
-    if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
-      let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
-        totalMovement = (panels.length - 1) * targetElem.offsetWidth;
-      y = Math.round(
-        tween.scrollTrigger.start +
-          (targetElem.offsetLeft / totalMovement) * totalScroll
-      );
-    }
-    gsap.to(window, {
-      scrollTo: {
-        y: y,
-        autoKill: false
-      },
-      duration: 1
-    });
-  });
-});
-
-/* Panels */
-const panels = gsap.utils.toArray("#panels-container .panel");
-tween = gsap.to(panels, {
-  xPercent: -100 * (panels.length - 1),
+let regionContainer = document.querySelector("#region-container");
+let tween;
+const region = gsap.utils.toArray("#region-container .panel");
+tween = gsap.to(region, {
+  xPercent: -100 * (region.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: "#panels-container",
+    trigger: "#region-container",
     pin: true,
     start: "top top",
     scrub: 1,
     snap: {
-      snapTo: 1 / (panels.length - 1),
+      snapTo: 1 / (region.length - 1),
       inertia: false,
       duration: { min: 0.1, max: 0.1 }
     },
-    end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
+    end: () => "+=" + (regionContainer.offsetWidth - innerWidth)
   }
 });
