9145646df8b0e27fbbba4f490efb6e2fe29ceba8
neodigm <3151842+neodigm@users.noreply.github.com>
2023-07-13 11:36:37 -0500

    enchantedCTA_border_issue_resolved

--- dist/neodigm55__v3_0.js
+++ dist/neodigm55__v3_0.js
@@ -36,7 +36,7 @@ neodigmWWInterval: false,
   N55_DEBUG_lOG: false,
   N55_AMPM_THEME: "light",
   N55_EVENT_HAPTIC: true,
-  N55_GENRE_MOTIF: "neodigm",  //  rainforest steampunk cyberpunk artdeco noir anime casino expressionist
+  N55_GENRE_MOTIF: "neodigm",  //  rainforest seasonal steampunk cyberpunk artdeco noir anime casino expressionist
   N55_THEME_DEFAULT: "brand",
   N55_THEME_COLORS: {"brand":["EDBA08","915E00"], "primary":["92a8d1","364C75"], "secondary":["EDCED0","978284"], "success":["009473","003817"], "white":["FFFFFF","FDFDFD"], "ghost":["ffffff","000000"],
    "danger":["DD4124","810000"], "warning":["F5DF4D","988200"], "info":["7BC4C4","1F6868"], "disabled":["868686","767676"], "night":["6a6a6a","242424"], "marcom":["B163A3","5F4B8B"], "party":["FF6F61","C93F60"]},
@@ -464,7 +464,7 @@ let neodigmTulip = new NeodigmTulip( document, ["neodigm-tulip", "[data-n55-tuli
 
 //  Neodigm 55 PopTart Begin  //
 class NeodigmPopTart {
-  constructor(_d, _aQ) {  //  Oscillation Overthruster
+  constructor(_d, _aQ) {  //  Orthogonal Diagonalizer
       this._d = _d; this._aQ = _aQ; this.sId = ""
       this.eSoda = this.eScrim = this.eClose = null
       this.fOnBeforeOpen = {}; this.fOnAfterOpen = {}; this.fOnClose = {}
@@ -1334,12 +1334,12 @@ function doDOMContentLoaded(){
   const neodigmMU = `
 <neodigm-sodapop-scrim></neodigm-sodapop-scrim>
 <neodigm-sodapop-scrim-close>
-    <svg fill="#DD4124" height="48" viewBox="1 0 22 22" width="48" data-sodapop-scrim-svg><path d="M0 0h24v24H0z" fill="none"></path><path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
+<svg fill="#DD4124" height="48" viewBox="1 0 22 22" width="48" data-sodapop-scrim-svg><path d="M0 0h24v24H0z" fill="none"></path><path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
 </neodigm-sodapop-scrim-close>
 <neodiigm-snack class="l-snackbar" role="alert">
-    <section id="js-snackbar__id" class="snackbar__cont snackbar__cont--hide" aria-live="polite" aria-atomic="true">
-        <div class="snackbar__progbar"></div><p class="snackbar__msg"></p>
-    </section>
+<section id="js-snackbar__id" class="snackbar__cont snackbar__cont--hide" aria-live="polite" aria-atomic="true">
+<div class="snackbar__progbar"></div><p class="snackbar__msg"></p>
+</section>
 </neodiigm-snack>
 <neodigm-tulip class="tulip__cont--hide" role="tooltip"><p></p> <neodigm-marquee data-n55-marquee-text=" ##msg## " data-n55-marquee-size="xsmall"><pre data-n55-theme="##theme##"></pre></neodigm-marquee> </neodigm-tulip>
 <neodigm-poptartclass=""></neodigm-poptart>`;  //  Universal Templs
