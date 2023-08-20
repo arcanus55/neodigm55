cc33be646b6515eb7a5070e6e039685165ae0baa
neodigm <3151842+neodigm@users.noreply.github.com>
2023-08-20 08:49:32 -0500

    wdgt_case_comp

--- dist/widgets/case_conversion_wdg/case_conversion_wdg.js
+++ dist/widgets/case_conversion_wdg/case_conversion_wdg.js
@@ -143,7 +143,7 @@ let doAction = function( sAct ){
 neodigmMarquee.init();
 neodigmEnchantedCTA.init();
 
-let aTiny = {"a":"ᵃ","b":"ᵇ","c":"ᶜ","d":"ᵈ","e":"ᵉ","f":"ᶠ","g":"ᵍ","h":"ʰ","i":"ᶦ","j":"ʲ","k":"ᵏ","l":"ᶫ","m":"ᵐ","n":"ᶰ","o":"ᵒ","p":"ᵖ","q":"ᑫ","r":"ʳ","s":"ˢ","t":"ᵗ","u":"ᵘ","v":"ᵛ","w":"ʷ","x":"ˣ","y":"ʸ","z":"ᶻ","A":"ᴬ","B":"ᴮ","C":"ᶜ","D":"ᴰ","E":"ᴱ","F":"ᶠ","G":"ᴳ","H":"ᴴ","I":"ᴵ","J":"ᴶ","K":"ᴷ","L":"ᴸ","M":"ᴹ","N":"ᴺ","O":"ᴼ","P":"ᴾ","Q":"ᑫ","R":"ᴿ","S":"ˢ","T":"ᵀ","U":"ᵁ","V":"ⱽ","W":"ᵂ","X":"ˣ","Y":"ʸ","Z":"ᶻ","`":"`","~":"~","!":"﹗","@":"@","#":"#","$":"﹩","%":"﹪","^":"^","&":"﹠","*":"﹡","(":"⁽",")":"⁾","_":"⁻","-":"⁻","=":"⁼","+":"+","{":"{","[":"[","}":"}","]":"]",":":"﹕",";":"﹔","?":"﹖"};
+let oTiny = {"a":"ᵃ","b":"ᵇ","c":"ᶜ","d":"ᵈ","e":"ᵉ","f":"ᶠ","g":"ᵍ","h":"ʰ","i":"ᶦ","j":"ʲ","k":"ᵏ","l":"ᶫ","m":"ᵐ","n":"ᶰ","o":"ᵒ","p":"ᵖ","q":"ᑫ","r":"ʳ","s":"ˢ","t":"ᵗ","u":"ᵘ","v":"ᵛ","w":"ʷ","x":"ˣ","y":"ʸ","z":"ᶻ","A":"ᴬ","B":"ᴮ","C":"ᶜ","D":"ᴰ","E":"ᴱ","F":"ᶠ","G":"ᴳ","H":"ᴴ","I":"ᴵ","J":"ᴶ","K":"ᴷ","L":"ᴸ","M":"ᴹ","N":"ᴺ","O":"ᴼ","P":"ᴾ","Q":"ᑫ","R":"ᴿ","S":"ˢ","T":"ᵀ","U":"ᵁ","V":"ⱽ","W":"ᵂ","X":"ˣ","Y":"ʸ","Z":"ᶻ","`":"`","~":"~","!":"﹗","@":"@","#":"#","$":"﹩","%":"﹪","^":"^","&":"﹠","*":"﹡","(":"⁽",")":"⁾","_":"⁻","-":"⁻","=":"⁼","+":"+","{":"{","[":"[","}":"}","]":"]",":":"﹕",";":"﹔","?":"﹖"};
 let doTinyCaption = ( (_d, _q, _t) => {  //  Inject Tiny type
     let aTinyCnt = [..._d.querySelectorAll( _q )];
     if( aTinyCnt ){ setTimeout( ()=>{ doTinyCaption.tick(); }, 56); }
@@ -153,7 +153,7 @@ let doTinyCaption = ( (_d, _q, _t) => {  //  Inject Tiny type
             aTinyCnt.forEach( (eVivCnt) => {
                 if(eVivCnt.atTiny !== eVivCnt.dataset.atTiny){ // Data atr changed
                     Array.from( eVivCnt.dataset.atTiny ).filter(( sChr )=>{
-                        sMU += ( sChr == " ") ? " "  : aTiny[ sChr ];
+                        sMU += ( sChr == " ") ? " "  : oTiny[ sChr ];
                     });
                     eVivCnt.innerHTML = sMU;
                     eVivCnt.atTiny = eVivCnt.dataset.atTiny;
@@ -167,7 +167,7 @@ let doTinyCaption = ( (_d, _q, _t) => {  //  Inject Tiny type
 let doTinyType = ( sIn ) =>{
     let sOut = "";
     Array.from( sIn ).filter(( sChr )=>{
-        sOut += ( sChr == " ") ? " "  : aTiny[ sChr ];
+        sOut += ( sChr == " ") ? " "  : oTiny[ sChr ];
     });
     return sOut;
 }
