4488f80015c454c84d1299a107bee6ff34ecf0e9
neodigm <3151842+neodigm@users.noreply.github.com>
2023-08-20 08:57:53 -0500

    wdgt_case_comp_tt

--- dist/widgets/case_conversion_wdg/case_conversion_wdg.js
+++ dist/widgets/case_conversion_wdg/case_conversion_wdg.js
@@ -73,7 +73,7 @@ let doAction = function( sAct ){
         }
         if( sAct == "caseTinyType" ){
             if( eTermFrom.value ){
-                eTermTo.value = doTinyType( eTermFrom.value )
+                eTermTo.value = getTinyType( eTermFrom.value )
                 neodigmToast.q("Text Convert|Tiny Type " + eTermFrom.value.length + " characters", "primary")
             }else{
                 neodigmToast.q("There is no text to convert.", "danger")
@@ -143,12 +143,15 @@ let doAction = function( sAct ){
 neodigmMarquee.init();
 neodigmEnchantedCTA.init();
 
-
-let doTinyType = ( sIn ) =>{
+let getTinyType = ( sIn ) =>{
     let oTiny = {"a":"ᵃ","b":"ᵇ","c":"ᶜ","d":"ᵈ","e":"ᵉ","f":"ᶠ","g":"ᵍ","h":"ʰ","i":"ᶦ","j":"ʲ","k":"ᵏ","l":"ᶫ","m":"ᵐ","n":"ᶰ","o":"ᵒ","p":"ᵖ","q":"ᑫ","r":"ʳ","s":"ˢ","t":"ᵗ","u":"ᵘ","v":"ᵛ","w":"ʷ","x":"ˣ","y":"ʸ","z":"ᶻ","A":"ᴬ","B":"ᴮ","C":"ᶜ","D":"ᴰ","E":"ᴱ","F":"ᶠ","G":"ᴳ","H":"ᴴ","I":"ᴵ","J":"ᴶ","K":"ᴷ","L":"ᴸ","M":"ᴹ","N":"ᴺ","O":"ᴼ","P":"ᴾ","Q":"ᑫ","R":"ᴿ","S":"ˢ","T":"ᵀ","U":"ᵁ","V":"ⱽ","W":"ᵂ","X":"ˣ","Y":"ʸ","Z":"ᶻ","`":"`","~":"~","!":"﹗","@":"@","#":"#","$":"﹩","%":"﹪","^":"^","&":"﹠","*":"﹡","(":"⁽",")":"⁾","_":"⁻","-":"⁻","=":"⁼","+":"+","{":"{","[":"[","}":"}","]":"]",":":"﹕",";":"﹔","?":"﹖"};
     let sOut = "";
     Array.from( sIn ).filter(( sChr )=>{
-        sOut += ( sChr == " ") ? " "  : oTiny[ sChr ];
+        if( oTiny[ sChr ] ){
+            sOut += oTiny[ sChr ]
+        }else{
+            sOut += " "
+        }
     });
     return sOut;
 }
