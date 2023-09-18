0ac7a9c5768dec0e2e309ae932e7f613f91158b7
neodigm <3151842+neodigm@users.noreply.github.com>
2023-09-18 09:23:41 -0500

    tulip_offset_8_to_10

--- dist/neodigm55__v3_1.js
+++ dist/neodigm55__v3_1.js
@@ -446,19 +446,19 @@ class NeodigmTulip {  //  Tooltip
     let nPrC = oRct.left + Math.round( nPrW / 2 )
     this.eTulip.style.top = oRct.top + "px";
     this.eTulip.style.left = nPrC - Math.round( nTlW / 2 ) + "px";
-    const NOFFSET = 16
+    const NOFFSET = 10
     switch( this.eTulip.dataset.n55Position = this.oCnfCur.position ){
       case "top":
         this.eTulip.style.top = oRct.top - (NOFFSET * NOFFSET) + "px";
       break
       case "right":
-        this.eTulip.style.left = ( oRct.right + NOFFSET ) + "px";
+        this.eTulip.style.left = ( oRct.right + (NOFFSET + 4) ) + "px";
       break
       case "bottom":
         this.eTulip.style.top = oRct.bottom + (NOFFSET + NOFFSET) + "px";
       break
       case "left":
-          this.eTulip.style.left = ( oRct.left - ( nTlW + NOFFSET ) ) + "px";
+          this.eTulip.style.left = ( oRct.left - ( nTlW + (NOFFSET + 4) ) ) + "px";
       break
     }
     this.bIsOpen = true
