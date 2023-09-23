cb0e7577a93dd069c70982f8748fd589a4456362
neodigm <3151842+neodigm@users.noreply.github.com>
2023-09-23 14:45:15 -0500

    issue_84

--- dist/neodigm55__v3_1.js
+++ dist/neodigm55__v3_1.js
@@ -57,7 +57,7 @@ if( typeof neodigmOptCustom != 'undefined' ){
 //  Neodigm 55 Utils Begin  //
 const neodigmUtils = ( ( _d ) =>{
   return {
-    ver: "3.1.0",  //  Neodigm 55 version
+    ver: "3.1.84",  //  Neodigm 55 version
     isMobile: function(){ return (_d.body.clientWidth <= 768) ? true : false; },
     isTouch: function(){ return (typeof document.body.ontouchstart != "undefined") },
     f1210: function(){ return (Math.floor(Math.random() * (10) + 1)); },  //  1 to 10
@@ -169,6 +169,7 @@ const neodigmUtils = ( ( _d ) =>{
       let elTrg = document.querySelector( o?.q1st )
       if( elTrg ){
         let aFraz = o.msg.split("|")
+        if( (o?.mode == "RANDOM" ) && aFraz.length ){ o.msg = aFraz[ neodigmUtils.f02x( aFraz.length ) ] }
         const NTIMES = [ o.msg.length, o.uniqueDelay ];
         neodigmUtils.typeOff({"q1st": o.q1st, "uniqueDelay": ( o.uniqueDelay / elTrg.textContent.length ) - 4 })
 
