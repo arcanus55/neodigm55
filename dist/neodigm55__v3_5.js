c546b1297325099bd27002523737d92b27aa6345
neodigm <3151842+neodigm@users.noreply.github.com>
2024-08-18 17:51:15 -0500

    utils_appStateList_KLM

--- dist/neodigm55__v3_5.js
+++ dist/neodigm55__v3_5.js
@@ -100,20 +100,20 @@ const neodigmUtils = ( ( _d ) =>{
       return ( isVal == "object" )
     },
     appStateListen: function( fCb ){  //  Update body atr, dataLayer, console log, and Session Storage
-      document[ neodigmOpt.N55_APP_STATE.CONTEXT ].addEventListener( "mouseover", ( ev ) =>{
-      //NeodigmKeylime.subscribe( "mouseover", ( ev )=>{
+      //document[ neodigmOpt.N55_APP_STATE.CONTEXT ].addEventListener( "mouseover", ( ev ) =>{
+      NeodigmKeylime.subscribe( "mouseover", ( ev )=>{
         if( ev?.target?.dataset?.n55TypeonHover ) neodigmUtils.typeOn( JSON.parse( ev.target.dataset.n55TypeonHover ) )
       }, true )
-      document[ neodigmOpt.N55_APP_STATE.CONTEXT ].addEventListener( "click", ( ev ) =>{
-      //NeodigmKeylime.subscribe( "click", ( ev )=>{
+      //document[ neodigmOpt.N55_APP_STATE.CONTEXT ].addEventListener( "click", ( ev ) =>{
+      NeodigmKeylime.subscribe( "click", ( ev )=>{
   console.log("~~~~~~~ click appState | " , ev )
         if( !neodigmOpt.N55_APP_STATE.FIRST_TAP ){ neodigmOpt.N55_APP_STATE.FIRST_TAP = true }
         if( neodigmOpt.neodigmTulip ) neodigmTulip.close() // TODO refact into class pub/sub emit?
         let evAtr = neodigmUtils.walkDOM3( ev?.target, "n55TypeonClick" )
         if( evAtr ) neodigmUtils.typeOn( JSON.parse( evAtr ) )
       }, true )
-      document[ neodigmOpt.N55_APP_STATE.CONTEXT ].addEventListener( "touchstart", ( ev ) =>{
-      //NeodigmKeylime.subscribe( "touchstart", ( ev )=>{
+      //document[ neodigmOpt.N55_APP_STATE.CONTEXT ].addEventListener( "touchstart", ( ev ) =>{
+      NeodigmKeylime.subscribe( "touchstart", ( ev )=>{
         if( !neodigmOpt.N55_APP_STATE.FIRST_TAP ){ neodigmOpt.N55_APP_STATE.FIRST_TAP = true }
         if( neodigmOpt.neodigmTulip ) neodigmTulip.close() // TODO refact into class pub/sub emit?
       }, true )
@@ -124,7 +124,7 @@ const neodigmUtils = ( ( _d ) =>{
           if( neodigmOpt.neodigmTulip ) neodigmTulip.close()
           if( neodigmOpt.neodigmPopTart ) neodigmPopTart.close()
         })
-      }, true )
+      } )
       window.addEventListener( "orientationchange", ( ev ) =>{ // TODO refact into class pub/sub emit?
       //NeodigmKeylime.subscribe( "orientationchange", ( ev )=>{
         window.requestAnimationFrame(() => {
@@ -132,13 +132,13 @@ const neodigmUtils = ( ( _d ) =>{
           if( neodigmOpt.neodigmTulip ) neodigmTulip.close()
           if( neodigmOpt.neodigmPopTart ) neodigmPopTart.close()
         })
-      }, true )
+      } )
       window.addEventListener( "scroll", ( ev ) =>{ // TODO refact into class pub/sub emit?
       //NeodigmKeylime.subscribe( "scroll", ( ev )=>{
         window.requestAnimationFrame(() => {
           if( neodigmOpt.neodigmTulip ) neodigmTulip.close()
         })
-      }, true )
+      } )
       neodigmOpt.N55_APP_STATE.REDUCE_MOTION = !window.matchMedia( '(prefers-reduced-motion: no-preference)' ).matches
       let sFirstAMPM = document[ neodigmOpt.N55_APP_STATE.CONTEXT ].querySelector( "[data-n55-Ampm-theme]" )?.dataset.n55AmpmTheme
       if( sFirstAMPM ) neodigmOpt.N55_AMPM_THEME = neodigmOpt.N55_APP_STATE.AMPM = sFirstAMPM
@@ -1906,9 +1906,9 @@ function doDOMContentLoaded(){
         elLk.setAttribute("href", neodigmOpt.N55_TYPE )
         document.head.appendChild( elLk )
     }
-    neodigmUtils.appStateListen()  //  Bind to Host
     neodigmMetronome.init()  //  Always-on
     NeodigmKeylime.init()  //  Static Always-on 
+    neodigmUtils.appStateListen()  //  Bind to Host
     NeodigmClaire.init()
     if( neodigmOpt.N55_AMPM_THEME && !document[ neodigmOpt.N55_APP_STATE.CONTEXT ].dataset.n55AmpmTheme ) document[ neodigmOpt.N55_APP_STATE.CONTEXT ].dataset.n55AmpmTheme = neodigmOpt.N55_AMPM_THEME
     if( neodigmOpt.CONSOLE_LOG_VER ) console.log("%c Neodigm 55 the eclectic low-code UX micro-library ✨ v" + neodigmUtils.ver, "background: #000; color: #F5DF4D; font-size: 20px");
