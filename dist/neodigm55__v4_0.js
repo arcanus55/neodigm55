cd031605397ef75513d867f73f227f752a43cde3
neodigm <3151842+neodigm@users.noreply.github.com>
2025-04-05 17:21:20 -0500

    n55 4.0 css pulse

--- dist/neodigm55__v4_0.js
+++ dist/neodigm55__v4_0.js
@@ -167,6 +167,12 @@ const neodigmUtils = ( ( _d ) =>{
             setTimeout( function(){ eC.dataset.n55Theme = eC.n55Theme }, ( nDx * (NDELAY + NDELAY) ) )            
           }
         } )
+        aE = [ ... document[ neodigmOpt.N55_APP_STATE.CONTEXT ].querySelectorAll("[data-n55-theme-pulse") ];; const NDELAYPULSE = 3000;
+        aE.forEach( ( eC, nDx ) => {
+          if( !eC.n55ThemePulse ) eC.n55ThemePulse = eC.dataset.n55ThemePulse
+          setTimeout( function(){ eC.dataset.n55ThemePulse = sTheme }, ( nDx * NDELAY ) )
+          setTimeout( function(){ eC.dataset.n55ThemePulse = eC.n55ThemePulse }, ( nDx * (NDELAY + NDELAY) ) )            
+        } )
         neodigmUtils.flashTitle( sTheme, 2e3 )
       }
     },
