1a8b7118f0718ca135350f5a69d89efaa5c4328c
neodigm <3151842+neodigm@users.noreply.github.com>
2025-07-04 18:21:46 -0500

    poptart open oPopTmpls update

--- dist/neodigm55__v4_0.js
+++ dist/neodigm55__v4_0.js
@@ -653,9 +653,13 @@ class NeodigmPopTart {
       }
     }
   }
+  autoOpen( elPop, oPos, elBound ) {
+    this.elBound = elBound
+    this.open( elPop, oPos )
+  }
   open( elPop, oPos ) {
     if( this.bIsInit && !this.bIsPause && elPop.id && !elPop.dataset?.n55PoptartOpen ) {
-        this.oPopTmpls[ elPop.id ] = elPop
+        this.oPopTmpls[ elPop.id ] = elPop 
         let nOffSetT, nOffSetL, nOffSetH, nOffSetW;  //  oPos offset conf
         nOffSetT = nOffSetL = nOffSetH = nOffSetW = 0;
         if( oPos?.offset ){
