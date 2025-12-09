2683554679c3d163dca28816d5b8456a055ae6ad
neodigm <3151842+neodigm@users.noreply.github.com>
2025-12-08 21:21:39 -0600

    neodigmOpt event_sound false - comment out

--- dist/neodigm55__v4_1.js
+++ dist/neodigm55__v4_1.js
@@ -64,7 +64,7 @@ if( typeof neodigmOptCustom != 'undefined' ){
 //  Neodigm 55 Utils Begin  //
 const neodigmUtils = ( ( _d ) =>{
   return {
-    ver: "4.1.0",  //  Neodigm 55 version
+    ver: "4.1.a",  //  Neodigm 55 version
     isMobile: function(){ return (_d.body.clientWidth <= 768) ? true : false; },
     isTouch: function(){ return (typeof document.body.ontouchstart != "undefined") },
     f1210: function(){ return (Math.floor(Math.random() * (10) + 1)); },  //  1 to 10
