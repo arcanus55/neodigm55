37bb05f9bc68cc66ff09d56f82469637a0389844
neodigm <3151842+neodigm@users.noreply.github.com>
2026-04-11 21:38:26 -0500

    x-m5t-brand header

--- dist/neodigm55__v5_0.js
+++ dist/neodigm55__v5_0.js
@@ -1052,6 +1052,7 @@ class mvvLegit {  //  Are you, you?
         if( url.toUpperCase().indexOf( mvvLegit.#conf.BASE.toUpperCase() ) != -1 ){  //  Verify that base URI is within the scope of that in conf
           if( bearerToken ) options.headers['Authorization'] = `Bearer ${bearerToken}`  // Add Authorization header with bearer token
           if( neodigmUtils ) options.headers['machvive'] = neodigmUtils?.ver || "Neodigm"
+          if( localStorage.getItem( "mvvBrand" ) ) options.headers['x-m5t-brand'] = localStorage.getItem( "mvvBrand" )
         }
         // Call original fetch with modified options
         return originalFetch(url, options).then(response => {
